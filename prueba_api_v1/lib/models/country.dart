6c5c5628228da43edac22d7780151a8b130fd7db
erikaquino <erik.aquino.ms@gmail.com>
2022-09-09 07:07:09 -0500

    Request a restcountries.com - prueba #2
    # Descripcion: Successful.
      Se obtienen los datos de la api de restcountries y se muestran en flutter.
    
    # Cambios:
    - lib/main: Se cambia la vista por defecto por una simple con texto. Esto permite visualizar menos código innecesario para pruebas en los archivos main.
    - Country.dart: Se agrega la clase Name, que permite obtener el los campos common y official de la api. También se cambian los constructores de la clase sin ser tipo Const
    -

--- prueba_api_v1/lib/models/country.dart
+++ prueba_api_v1/lib/models/country.dart
@@ -1,40 +1,22 @@
-// import 'package:flutter/cupertino.dart';
+class Name {
+  String common;
+  String official;
+  Name(this.common, this.official);
+  factory Name.fromJson(dynamic json) {
+    return Name(json['common'], json['official']);
+  }
+}
 
 class Country {
-  // Properties
-  final String common;
+  // Propiedades que se quieren del request a la api
+  final Name name;
   final String cca2;
   final String flag;
   // Constructor
-  const Country({
-    required this.common,
-    required this.cca2,
-    required this.flag,
-  });
-  //Country({this.name = '', this.cca2 = '', this.capital = '', this.flag = ''});
+  Country(this.name, this.cca2, this.flag);
 
   //retorna una instancia de pais
-  // Tasks:
-  // 1. Crear un constructor que me permita hacer una instancia de pais y que muestre la lista
-  factory Country.fromJson(Map<String, dynamic> json) =>
-    Country(
-        common: json['name'],
-        cca2: json['cca2'],
-        flag: json['flag'],
-      );
-
-  //1. CODIGO AGREGADO RECIENTEMENTE (TAMPOCO FUNCIONA)
-  /*Country.fromJson(Map<String, dynamic> json) {
-     return Country(
-       common: json ['common'],
-       cca2: json ['cca2'],
-       flag: json ['flag'],
-     );
-   }*/
-
-  //2. OTRA OPCION PARA CONSUMIR API
-
-  /* String toString() {
-     return 'Instance of Country: $common';
-   }*/
+  factory Country.fromJson(dynamic json) {
+    return Country(Name.fromJson(json['name']), json['cca2'], json['flag']);
+  }
 }
--- prueba_api_v1/lib/services/country_service.dart
+++ prueba_api_v1/lib/services/country_service.dart
@@ -1,55 +1,47 @@
-import 'dart:html';
-import 'package:dio/dio.dart';
-import 'package:prueba_api_v1/models/country.dart';
-
-import 'dart:convert'; // required by: jsonDecode
+import 'dart:convert'; // sirve para: jsonDecode()
+// import 'package:dio/dio.dart';  no voy a usar dio para esta prueba
+// import 'package:flutter/material.dart'; tampoco usaré material aqui
 import 'package:http/http.dart' as http; // similar a dio.dart
-import 'package:prueba_api_v1/models/country_response'; // Para la opcion 2
+import 'package:prueba_api_v1/models/country.dart';
+import 'package:prueba_api_v1/models/country_responses.dart'; // Para la opcion 2
 
 class CountryService {
-  // Opción 1. utilizando getCountryByName con dio.dart (no pude correr dio :(
-
-  // Properties
-  List<Country> search;
-  // Constructor
-  CountryService({required this.search});
   //
-  Future getCountryByName(String name) async {
-    try {
-      // Hay dos opciones para tener los detalles desde https://restcountries.com/#api-endpoints-v3-name
-      // 1. https://restcountries.com/v3.1/all
-      // 2. https://restcountries.com/v3.1/name/peru
-      // aqui estaba escrito junto al final, decia: '/all/$name'
-      // Entonces, se llama un pais
-      final url = 'https://restcountries.com/v3.1/name/$name';
-      final resp = await _dio.get(url);
-      List<dynamic> countryList = resp.data;
-      // Crea lista con paises ya instanciados
-      List<Country> search = countryList.map((dynamic value) => Country.fromJson(value)).toList();
-      return CountryService(search: search);
-      //print(resp.data);
-      //return [];
-    } catch (e) {
-      print(e);
-      return [];
+  // Opción 1. Short version: Crea paises de un request
+
+  Future<Country> getCountryByName(String countryReq) async {
+    final resp = await http
+        .get(Uri.parse("https://restcountries.com/v3.1/name/$countryReq"));
+    if (resp.statusCode == 200) {
+      List<dynamic> jsonDecoded = jsonDecode(resp.body);
+      // Los datos viende dentro de array: [{jsondata}]. Se extrae:
+      var result = jsonDecoded[0];
+      // Se crea una instancia con los datos que vienen:
+      Country country = Country.fromJson(result);
+      // Se puede simplificar a return Country.fromJson(jsonDecode(resp.body)[0]); pero menos leible
+      return country;
+    } else {
+      throw Exception('Failed to load album');
     }
   }
 
-  // Opción 2. utilizando http.dart
-  //
+  // Opción 2. Complet version: Verifica servidor, batch y crea lista
+
+  // Convierte json a Country class
   static Future<CountrySearchResponse?> query(String search) async {
-    // similar a dio.get
+    // similar a dio.get que usabas
     var response = await http
         .get(Uri.parse("https://restcountries.com/v3.1/name/$search"));
-    // Revisa el estatus antes de crear la lista de paises
+    // Revisa la respuesta del servidor antes de crear la lista de paises
     if (response.statusCode >= 200 && response.statusCode < 300) {
       // convierte de string a json
       var map = json.decode(response.body);
       // crea la lista de nombres
       return CountrySearchResponse.fromJson(map);
     } else {
-      print("Query failed: ${response.body} (${response.statusCode})");
-      return null;
+      // Si el servidor no respondio 200 ok, muestra el status
+      throw Exception('No se pudieron cargar los paises.');
+      // print("Query failed: ${response.body} (${response.statusCode})");
     }
   }
 }
