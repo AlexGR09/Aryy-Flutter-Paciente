2240713b67f89faa89f4e7ebb0892de6f14e3ed2
alethiaimperia <alethiaimperia@gmail.com>
2022-10-07 14:38:26 -0500

    construccion de vista configurar pin y datos biometricos

--- lib/configurar_pin_datosbiometricos/configurar_pin_datosbiometricos_widget.dart
+++ lib/configurar_pin_datosbiometricos/configurar_pin_datosbiometricos_widget.dart
@@ -2,6 +2,7 @@ import 'package:aryy_front/styles/my_icons.dart';
 
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
+import '../flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -22,18 +23,19 @@ class _ConfigurarPinDatosbiometricosWidgetState
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
+      backgroundColor: FlutterFlowTheme.of(context).secondaryBackground,
       appBar: AppBar(
         backgroundColor: FlutterFlowTheme.of(context).secondaryBackground,
         automaticallyImplyLeading: false,
         title: Padding(
-          padding: const EdgeInsetsDirectional.fromSTEB(22, 0, 0, 0),
+          padding: EdgeInsetsDirectional.fromSTEB(22, 0, 0, 0),
           child: Row(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               InkWell(
                 onTap: () {
-                  Navigator.pushNamed(context, "codigo_verificacion");
+                  Navigator.pushNamed(context, 'codigo_verificacion');
                 },
                 child: SvgPicture.asset(
                   REGRESAR,
@@ -43,15 +45,15 @@ class _ConfigurarPinDatosbiometricosWidgetState
                 ),
               ),
               Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 6, 0),
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 6, 0),
                 child: InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, "iniciarsesion");
+                  onTap: () async {
+                    context.pushNamed('iniciarsesion');
                   },
                   child: Text(
                     'Omitir',
                     style: FlutterFlowTheme.of(context).bodyText1.override(
-                          fontFamily: 'Poppins',
+                          fontFamily: 'Montserrat',
                           color: Color(0xC586898C),
                           fontWeight: FontWeight.normal,
                         ),
@@ -65,7 +67,6 @@ class _ConfigurarPinDatosbiometricosWidgetState
         centerTitle: false,
         elevation: 0,
       ),
-      backgroundColor: FlutterFlowTheme.of(context).secondaryBackground,
       body: SafeArea(
         child: GestureDetector(
           onTap: () => FocusScope.of(context).unfocus(),
@@ -73,7 +74,7 @@ class _ConfigurarPinDatosbiometricosWidgetState
             mainAxisSize: MainAxisSize.max,
             children: [
               Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(22, 60, 22, 0),
+                padding: EdgeInsetsDirectional.fromSTEB(22, 60, 22, 0),
                 child: Row(
                   mainAxisSize: MainAxisSize.max,
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -182,8 +183,9 @@ class _ConfigurarPinDatosbiometricosWidgetState
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         InkWell(
-                          onTap: () async {
-                            context.pushNamed('sesion_huella_dactilar');
+                          onTap: () {
+                            Navigator.pushNamed(
+                                context, 'sesion_huella_dactilar');
                           },
                           child: Row(
                             mainAxisSize: MainAxisSize.max,
@@ -202,7 +204,7 @@ class _ConfigurarPinDatosbiometricosWidgetState
                                   style: FlutterFlowTheme.of(context)
                                       .bodyText1
                                       .override(
-                                        fontFamily: 'Poppins',
+                                        fontFamily: 'Montserrat',
                                         color: Color(0xFF5101CC),
                                         fontWeight: FontWeight.w500,
                                       ),
@@ -213,38 +215,69 @@ class _ConfigurarPinDatosbiometricosWidgetState
                         ),
                         Padding(
                           padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              InkWell(
-                                onTap: () async {
-                                  context.pushNamed('sesion_huella_dactilar');
-                                },
-                              ),
-                              SvgPicture.asset(
-                                FACE,
-                                width: 40,
-                                height: 40,
-                                fit: BoxFit.cover,
-                              ),
-                              Padding(
-                                padding:
-                                    EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
-                                child: Text(
-                                  'Registrar rostro',
-                                  style: FlutterFlowTheme.of(context)
-                                      .bodyText1
-                                      .override(
-                                        fontFamily: 'Montserrat',
-                                        color: Color(0xFF5101CC),
-                                        fontWeight: FontWeight.w500,
-                                      ),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(
+                                  context, 'sesion_huella_dactilar');
+                            },
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                SvgPicture.asset(
+                                  FACE,
+                                  width: 40,
+                                  height: 40,
+                                  fit: BoxFit.cover,
                                 ),
-                              ),
-                            ],
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      20, 0, 0, 0),
+                                  child: Text(
+                                    'Registrar rostro',
+                                    style: FlutterFlowTheme.of(context)
+                                        .bodyText1
+                                        .override(
+                                          fontFamily: 'Montserrat',
+                                          color: Color(0xFF5101CC),
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 20),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FFButtonWidget(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'iniciarsesion');
+                      },
+                      text: 'aceptar',
+                      options: FFButtonOptions(
+                        width: 130,
+                        height: 40,
+                        color: FlutterFlowTheme.of(context).primaryBtnText,
+                        textStyle:
+                            FlutterFlowTheme.of(context).subtitle2.override(
+                                  fontFamily: 'Montserrat',
+                                  color: Color(0xFF7900FF),
+                                ),
+                        borderSide: BorderSide(
+                          color: Color(0xFF7900FF),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
                   ],
                 ),
