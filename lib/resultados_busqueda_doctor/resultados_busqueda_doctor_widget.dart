faf0a729fb9cb25e84d2e131afc38ba90af1d462
erikaquino <112906783+erikaquinoms@users.noreply.github.com>
2022-10-14 17:54:42 -0500

    Se completa la integracion de la nueva vista

--- lib/resultados_busqueda_doctor/resultados_busqueda_doctor_widget.dart
+++ lib/resultados_busqueda_doctor/resultados_busqueda_doctor_widget.dart
@@ -1,11 +1,11 @@
+import '../flutter_flow/app_state.dart';
 import '../flutter_flow/flutter_flow_icon_button.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
-import '../flutter_flow/flutter_flow_util.dart';
 import '../flutter_flow/flutter_flow_widgets.dart';
+import '../flutter_flow/togleIcon.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomePageWidget extends StatefulWidget {
   const HomePageWidget({
