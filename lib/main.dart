1f231ec4ad99683f843c1de871d84b59d613fbf6
erikaquino <112906783+erikaquinoms@users.noreply.github.com>
2022-12-06 17:35:03 -0600

    Vista: Historial vacunación por componenetes

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
           return MaterialApp(
               debugShowCheckedModeBanner: false,
               title: 'Aryy - Prueba',
-              initialRoute: "menu_expediente",
+              initialRoute: "historial_vacunacion",
               // Pruebas responsivas y modo oscuro
               theme: ThemeData.light(),
               darkTheme: ThemeData.dark(),
@@ -80,8 +80,9 @@ class MyApp extends StatelessWidget {
                 "informacion_basica": ((context) =>
                     const InformacionBasicaWidget()),
                 "menu_expediente": (_) => MenuExpedienteWidget(),
+                "historial_vacunacion": (_) => HistorialVacunacionWidget(),
               },
-              home: MenuExpedienteWidget());
+              home: HistorialVacunacionWidget());
         });
   }
 }
